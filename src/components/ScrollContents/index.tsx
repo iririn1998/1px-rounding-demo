ffdb20a466a52b28cb6fd30d95532bd46e17cc5c
iririn1998 <iririn199810710@gmail.com>
2026-07-27 23:07:02 +0900

    style: ScrollContentsにitemクラスを追加してテキストを中央揃え

--- src/components/ScrollContents/index.tsx
+++ src/components/ScrollContents/index.tsx
@@ -4,9 +4,9 @@ import styles from "./index.module.css";
 export const ScrollContents: FC = () => {
   return (
     <div className={styles["scroll-contents"]}>
-      <span>コンテンツ１</span>
-      <span>コンテンツ２</span>
-      <span>コンテンツ３</span>
+      <span className={styles["item"]}>コンテンツ１</span>
+      <span className={styles["item"]}>コンテンツ２</span>
+      <span className={styles["item"]}>コンテンツ３</span>
     </div>
   );
 };
